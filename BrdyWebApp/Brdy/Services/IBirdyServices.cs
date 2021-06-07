0c8b14c9a797927d89ca294c3754600077bb12a2
Ifeakandu Okoye <iokoye217@gmail.com>
2021-06-07 19:40:26 -0400

    push

---
+++ BrdyWebApp/Brdy/Services/IBirdyServices.cs
@@ -0,0 +1,11 @@
+﻿using Brdy.Models;
+using System.Threading.Tasks;
+
+namespace Brdy.Services
+{
+    public interface IBirdyServices
+    {
+        Task<SightingDetail> GetLocationAsync(string locName);
+        Task<SightingDetail> GetSpeciesAsync(string comName);
+    }
+}
