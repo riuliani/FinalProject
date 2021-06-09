891f8328edcad3d0b5836b3fc3c45f0d92db9a75
Darrius horton <darriushorton@Darrius-Air.attlocal.net>
2021-06-09 09:21:37 -0400

    Fixed styling convention working on api recent sighting

--- BrdyWebApp/Brdy/Models/SightingDetail.cs
+++ BrdyWebApp/Brdy/Models/SightingDetail.cs
@@ -9,14 +9,19 @@ namespace Brdy.Models
     public class SightingDetail
     {
         public string speciesCode { get; set; }
+        [Display(Name = "Common Name")]
         public string comName { get; set; }
+        [Display(Name = "Scientific Name")]
         public string sciName { get; set; }
+        [Display(Name = "Location")]
         public string locId { get; set; }
         [Display(Name = "") ]
         public string locName { get; set; }
         public string obsDt { get; set; }
         public int howMany { get; set; }
+        [Display(Name = "Lat")]
         public float lat { get; set; }
+        [Display(Name = "Long")]
         public float lng { get; set; }
         public bool obsValid { get; set; }
         public bool obsReviewed { get; set; }
